a10fb4baf81541e6026ead0c8aa602edd56ad5b2
Esther <151816213+SterTheStar@users.noreply.github.com>
2025-05-22 11:40:53 -0300

    Update alpine-setup.sh

--- alpine-setup.sh
+++ alpine-setup.sh
@@ -2,5 +2,4 @@
 cp /etc/apk/repositories /etc/apk/repositories.bak
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
 apk update
-apk add bash sudo
 echo "Repositório community adicionado e pacotes instalados."
